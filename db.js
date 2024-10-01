e11b4cf7bfb0d55c78ca53ad45fb044bf3eea835
BIKRAMSINGH1221 <bikomaster9459@gmail.com>
2024-10-01 19:58:54 +0530

    Changes in Database

--- db.js
+++ db.js
@@ -1,15 +1,21 @@
-import pg  from "pg";
+import pg from "pg"
+import env from "dotenv";
 
-const db = new pg.Client({
-    user : "postgres",
-    host : "localhost",
-    database : "postgres",
-    password : "mohan",
-    port : 5432
+const { Client } = pg;
+env.config();
+const db = new Client({
+  // connectionString: process.env.DB_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_POST,
+  ssl: {
+    rejectUnauthorized: false // Set to true if you want to reject self-signed certificates
+  }
 });
 
 db.connect();
-
 
 
 
