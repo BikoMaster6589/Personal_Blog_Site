1350e22f6b0af66e766524cc28bab117474e0f5c
BIKRAMSINGH1221 <bikomaster9459@gmail.com>
2024-10-03 05:36:59 +0530

    Added Correct google url

--- index.js
+++ index.js
@@ -539,7 +539,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets", // Use HTTP for local development
+      callbackURL: "https://your-app-name.onrender.com/auth/google/callback/auth/google/secrets", // Use HTTP for local development
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
