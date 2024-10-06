c5eb4a2a7235ea6231bf9f6b02523733b5b4e973
BIKRAMSINGH1221 <bikomaster9459@gmail.com>
2024-10-06 17:59:05 +0530

    Changes made

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ const storage = multer.diskStorage({
 
 
 const upload = multer({ 
-    storage: storage 
-    limits: { fileSize: 5 * 1024 * 1024 };
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 }
   });
 
 const transporter = nodemailer.createTransport({
